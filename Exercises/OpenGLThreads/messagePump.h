9bb0102797777b29d6619af20953ae77ae186dc1
SimWNielsen@hotmail.com <SimWNielsen@hotmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-09-15 10:05:36 +0000

    New structure. Not complete right now

---
+++ Exercises/OpenGLThreads/messagePump.h
@@ -0,0 +1,32 @@
+#include <SDL.h>
+#include <string>
+#include <list>
+#include <map>
+
+using namespace std;
+
+class MessagePump								// Singleton
+{
+public:
+	
+	static MessagePump _instance;
+
+	MessagePump() { &getInstance(); }
+	~MessagePump() { } 
+	MessagePump(const MessagePump &getInstance());   
+	
+	MessagePump & operator=(MessagePump &getInstance());
+	static MessagePump &getInstance();
+	static void sendMessage(SDL_Event msg);
+	static void sendPriorityMessage(SDL_Event msg);
+	static SDL_Event receiveMessage();
+	static SDL_Event readMessage();
+	static SDL_Event receiveLastMessage();
+	static SDL_Event readLastMessage();
+	static void deleteMessage();
+	static void deleteLastMessage();
+
+	// external declaration
+	static list<SDL_Event> messageList;
+	
+};
