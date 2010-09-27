47ed360ffdee9d0addd9f2dbbae62fa07a6b0b03
SimWNielsen@hotmail.com <SimWNielsen@hotmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-09-27 10:45:43 +0000

    basics should work now.

--- Exercises/OpenGL4/OpenGL4/inputManager.h
+++ Exercises/OpenGL4/OpenGL4/inputManager.h
@@ -1,12 +1,13 @@
 #include <SDL.h>
-//#include "messagePump.h"
+#include "messagePump.h"
 
 class inputManager
 {
 public:
+	int update();
 
 private:
 	void keyPress(SDL_Event currentEvent);
 	void mousePress(SDL_Event currentEvent);
 	void mouseMotion(SDL_Event currentEvent);
-}
+};
