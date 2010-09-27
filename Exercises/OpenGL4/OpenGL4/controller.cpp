f238cfb5ef9789286038245cccdcf0d632f04be3
SimWNielsen@hotmail.com <SimWNielsen@hotmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-09-27 14:58:54 +0000

    basic declarations of camera, spectator and controller are now in

--- Exercises/OpenGL4/OpenGL4/controller.cpp
+++ Exercises/OpenGL4/OpenGL4/controller.cpp
@@ -3,8 +3,46 @@
 // Declare static variables
 Controller Controller::_instance;
 SDL_mutex * Controller::mutex_controller;
+entitySpectator * Controller::playerObject;
+
+
 
 Controller &Controller::getInstance()
 {
   return _instance;
+}
+
+void Controller::setPlayerObject(entitySpectator *player)
+{
+	playerObject = player;
+}
+
+void Controller::moveForward(bool shouldMove)
+{
+
+}
+
+void Controller::moveBackward(bool shouldMove)
+{
+
+}
+
+void Controller::strafeLeft(bool shouldMove)
+{
+
+}
+
+void Controller::strafeRight(bool shouldMove)
+{
+
+}
+
+void Controller::lookUpDown(float deltaLook)
+{
+
+}
+
+void Controller::lookLeftRight(float deltaLook)
+{
+
 }
--- Exercises/OpenGL4/OpenGL4/controller.h
+++ Exercises/OpenGL4/OpenGL4/controller.h
@@ -1,18 +1,26 @@
 #include <SDL.h>
+#include "entitySpectator.h"
 
 class Controller								// Singleton
 {
 public:
 	static Controller _instance;
 	static SDL_mutex *mutex_controller;
+	static entitySpectator *playerObject;
  
-	Controller() { &getInstance(); mutex_controller = SDL_CreateMutex(); }
+	Controller() { mutex_controller = SDL_CreateMutex(); playerObject = NULL; &getInstance(); }
 	~Controller() { SDL_DestroyMutex( mutex_controller ); } 
-	Controller(const Controller &getInstance());   
+	Controller(const Controller &getInstance());
 	
 	Controller & operator=(Controller &getInstance());
 	static Controller &getInstance();
-
+	static void setPlayerObject(entitySpectator *player);
+	static void moveForward(bool shouldMove);
+	static void moveBackward(bool shouldMove);
+	static void strafeLeft(bool shouldMove);
+	static void strafeRight(bool shouldMove);
+	static void lookUpDown(float deltaLook);
+	static void lookLeftRight(float deltaLook);
 	static void lock() { SDL_mutexP( mutex_controller ); }
-	static void unlock() { SDL_mutexV( mutex_controller ); }	
+	static void unlock() { SDL_mutexV( mutex_controller ); }
 };
