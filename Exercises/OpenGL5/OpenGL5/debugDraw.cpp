480cb16bf36fcd8b9308cb4df6f0a595c4e9a6a4
valeriodidonato@gmail.com <valeriodidonato@gmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-10-11 20:11:33 +0000

    implementation of drawDebug abstract methods finished

--- Exercises/OpenGL5/OpenGL5/debugDraw.cpp
+++ Exercises/OpenGL5/OpenGL5/debugDraw.cpp
@@ -10,13 +10,38 @@ DebugDraw::~DebugDraw(void)
 {
 }
 
-void DebugDraw::drawLine(const btVector3& from, const btVector3& to) 
+void DebugDraw::drawLine(const btVector3& from, const btVector3& to, const btVector3 &color) 
 { 
+	// color not used
+
 	glColor3f(1.0, 1.0, 1.0);
 	glBegin(GL_LINES);
 		glVertex3f(from.getX(), from.getY(), from.getZ());
 		glVertex3f(to.getX(), to.getY(), to.getZ());
 	glEnd();
+} 
+
+void DebugDraw::drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color) 
+{ 
+
+	const btVector3 to(PointOnB + normalOnB*distance); 
+
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_LINES);
+		glVertex3f(PointOnB.getX(), PointOnB.getY(), PointOnB.getZ());
+		glVertex3f(to.getX(), to.getY(), to.getZ());
+	glEnd();
+
+} 
+
+void DebugDraw::reportErrorWarning(const char* text) 
+{ 
+	// do nothing for now 
+} 
+
+void DebugDraw::draw3dText(const btVector3& location, const char* text) 
+{ 
+	// do nothing for now 
 } 
 
 void DebugDraw::setDebugMode(int mode) 
