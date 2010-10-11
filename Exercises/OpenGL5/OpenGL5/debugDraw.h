657e12ae8360cec911d8efd4463522531d9cb056
valeriodidonato@gmail.com <valeriodidonato@gmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-10-11 21:18:20 +0000

    drawDebugger updates

--- Exercises/OpenGL5/OpenGL5/debugDraw.h
+++ Exercises/OpenGL5/OpenGL5/debugDraw.h
@@ -10,7 +10,7 @@ public:
 
 	void DebugDraw::drawLine(const btVector3& from, const btVector3& to, const btVector3 &color);
 	void DebugDraw::setDebugMode(int mode);
-	int DebugDraw::getDebugMode(void) const;
+	int  DebugDraw::getDebugMode(void) const;
 	void DebugDraw::drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color);
 	void DebugDraw::reportErrorWarning(const char* text);
 	void DebugDraw::draw3dText(const btVector3& location, const char* text);
