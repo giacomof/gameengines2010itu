5577cd8cb48687f8e6e59521fdc6ac652109fd58
valeriodidonato@gmail.com <valeriodidonato@gmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-10-11 16:43:40 +0000

    a debugDraw snippet added

--- Exercises/OpenGL5/OpenGL5/debugDraw.cpp
+++ Exercises/OpenGL5/OpenGL5/debugDraw.cpp
@@ -1,5 +1,5 @@
 #include "debugDraw.h"
-
+#include "SDL_opengl.h"
 
 DebugDraw::DebugDraw(void)
 {
@@ -9,3 +9,8 @@ DebugDraw::DebugDraw(void)
 DebugDraw::~DebugDraw(void)
 {
 }
+
+void drawLine(const btVector3& from, const btVector3& to, const btVector3& color) 
+{ 
+ 
+} 
