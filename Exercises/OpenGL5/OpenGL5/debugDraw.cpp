30f209817449fb1cf14c4942d2d8a7dad63cd189
valeriodidonato@gmail.com <valeriodidonato@gmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-10-11 20:03:41 +0000

    minor changes to debugDraw

--- Exercises/OpenGL5/OpenGL5/debugDraw.cpp
+++ Exercises/OpenGL5/OpenGL5/debugDraw.cpp
@@ -10,7 +10,21 @@ DebugDraw::~DebugDraw(void)
 {
 }
 
-void drawLine(const btVector3& from, const btVector3& to, const btVector3& color) 
+void DebugDraw::drawLine(const btVector3& from, const btVector3& to) 
 { 
- 
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_LINES);
+		glVertex3f(from.getX(), from.getY(), from.getZ());
+		glVertex3f(to.getX(), to.getY(), to.getZ());
+	glEnd();
+} 
+
+void DebugDraw::setDebugMode(int mode) 
+{ 
+	this->mode = mode; 
+} 
+
+int DebugDraw::getDebugMode(void) const 
+{ 
+	return this->mode; 
 } 
