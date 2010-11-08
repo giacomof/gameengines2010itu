d8a833e01926fbba27e5d057815a5d458cb27be4
valeriodidonato@gmail.com <valeriodidonato@gmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-11-08 15:27:43 +0000

    skeleton is now optional for colladas, memory manager with freetolastmarker now working

--- Exercises/OpenGL5/OpenGL5/memoryManager.cpp
+++ Exercises/OpenGL5/OpenGL5/memoryManager.cpp
@@ -7,6 +7,7 @@ MemoryManager MemoryManager::_instance;
 unsigned int MemoryManager::count=0;
 unsigned int MemoryManager::marker=0;
 unsigned int MemoryManager::lastMarker=0;
+list<unsigned int> * MemoryManager::markersList;
 
 void * MemoryManager::operator new(size_t s) {
 	
@@ -17,7 +18,7 @@ void * MemoryManager::operator new(size_t s) {
 
 void MemoryManager::operator delete(void *){ }
 
-MemoryManager::MemoryManager(void) { 
+MemoryManager::MemoryManager(void) {
 	&getInstance(); 
 }
 
@@ -25,35 +26,34 @@ MemoryManager::~MemoryManager(void) { }
 
 MemoryManager & MemoryManager::getInstance()
 {
-	if(count==0) {
-		marker = lastMarker = (unsigned int) malloc(dataToAllocate);
-		if((void *)marker == NULL) std::cout << "ERROR, NOT ENOUGH MEMORY" << std::endl;
+	if(MemoryManager::count==0) {
+		markersList = new list<unsigned int>();
+		std::cout << "pointer to list: " << &markersList << std::endl;
+
+		unsigned int tempMark = (unsigned int) malloc(dataToAllocate);
+		markersList->push_back(tempMark);
+		if(markersList->back() == NULL) std::cout << "ERROR, NOT ENOUGH MEMORY" << std::endl;
 	}
-	else count++;
+	else MemoryManager::count++;
+
 	return _instance;
 }
 
 unsigned int MemoryManager::getMarker(void) {
-	return marker;
-}
-
-void MemoryManager::setMarker(unsigned int m) {
-	marker = m;
+	return markersList->back();
 }
 
 void * MemoryManager::allocate(unsigned int s) 
 {
-	// save the last marker
-	lastMarker = marker;
 	// add the space required to the marker
-	marker = marker+s;
+	markersList->push_back(markersList->back()+s);
 	// return the pointer to the position of the marker
-	return (void *) marker;
+	return (void *) markersList->back();
 }
 
 void MemoryManager::freeToLastMarker(void) 
 {
-	marker = lastMarker;
+	markersList->pop_back();
 }
 
 void MemoryManager::deallocate(void * stack_ptr)
