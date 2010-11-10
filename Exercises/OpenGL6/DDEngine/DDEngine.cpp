7b152809d91f27c6ecf98afaed6c663e944a40a2
telivyel@gmail.com <telivyel@gmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-11-10 14:55:09 +0000

    WIP on DDEngine class

--- Exercises/OpenGL6/DDEngine/DDEngine.cpp
+++ Exercises/OpenGL6/DDEngine/DDEngine.cpp
@@ -1,8 +1,51 @@
 #include "DDEngine.h"
 
+static int const thread_delay = 1;			// Minimum time between loops
+static float const PI = 3.14159f;			// PI definition
 
-DDEngine::DDEngine(void)
+
+
+DDEngine::DDEngine(int scrW, int scrH, int colorD)
 {
+	screenWidth = scrW;						// Window Width
+	screenHeight = scrH;					// Window Height
+	screenColorDepth = colorD;				// Color Depth
+
+	// Create the asset manager
+	assetManagerPtr = AssetManager();
+	// Create the input pump
+	InputPump = MessagePump();
+	// Create the window manager
+	WindowManager window = WindowManager();
+	// Create the input abstraction layer
+	controller = Controller();
+
+	// SDL initialization
+	if (SDL_Init(SDL_INIT_VIDEO)<0)
+	{
+		exit(1);
+	}
+
+	// Create the window
+	window.createWindow(screenWidth, screenHeight, screenColorDepth);
+
+	// Set rendering parameters for OpenGL
+	if (initGL()==FALSE)
+	{
+		
+		exit(1);
+	}
+
+	// Resize OpenGL window
+	window.resizeWindow(screenWidth, screenHeight);
+
+	// Create the root node
+	rootNode = Root();
+	rootNode.setName("root");
+
+	// Define the exit of the program in relation to SDL variables
+	atexit(SDL_Quit);
+
 
 }
 
@@ -10,4 +53,35 @@ DDEngine::DDEngine(void)
 DDEngine::~DDEngine(void)
 {
 
+}
+
+
+int DDEngine::initGL(void)
+{
+	//glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+
+	// Clears color buffer
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	// sets the matrix stack as the projection matrix stack
+	glMatrixMode(GL_PROJECTION);
+	// creates the viewport
+	glViewport(0, 0, screenWidth, screenHeight);
+	glLoadIdentity();
+	// sets the matrix stack as the modelview matrix stack
+	glMatrixMode(GL_MODELVIEW);
+	gluPerspective(60.0f, screenWidth/screenHeight, 0.1f, 5000.0f);
+
+	// enables the Z-buffer
+	glEnable(GL_DEPTH_TEST);
+	// enables the texture rendering
+	glEnable(GL_TEXTURE_2D);
+	// enables smooth shading (garaud)
+	glShadeModel(GL_SMOOTH);
+	// frontfacing culling
+	glEnable(GL_CULL_FACE);
+	// enables lighting
+	glEnable(GL_LIGHTING);
+	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+	glColorMaterial(GL_FRONT_AND_BACK, GL_SPECULAR);
+	glEnable(GL_COLOR_MATERIAL);
 }
--- Exercises/OpenGL6/DDEngine/DDEngine.h
+++ Exercises/OpenGL6/DDEngine/DDEngine.h
@@ -7,29 +7,29 @@
 #ifndef DDEngine__H 
 #define DDEngine__H
 
-#include <iostream>						// Header File For the basic Input/Output system
-#include <windows.h>					// Header File For Windows
-#include <stdio.h>						// Header File For Standard Input/Output
-#include <stdlib.h>						// Header File For the STD library
-#include <SDL.h>						// Header File for the SDL library
-#include <SDL_opengl.h>					// Header File for OpenGL through SDL
-#include <SDL_thread.h>					// Header File for SDL thread library
-#include <SDL_audio.h>					// Header File for SDL audio library
-#include <glut.h>						// Header File for glut commands
-#include <btBulletDynamicsCommon.h>		// Header File for Bullet Physics
+#include <iostream>								// Header File For the basic Input/Output system
+#include <windows.h>							// Header File For Windows
+#include <stdio.h>								// Header File For Standard Input/Output
+#include <stdlib.h>								// Header File For the STD library
+#include <SDL.h>								// Header File for the SDL library
+#include <SDL_opengl.h>							// Header File for OpenGL through SDL
+#include <SDL_thread.h>							// Header File for SDL thread library
+#include <SDL_audio.h>							// Header File for SDL audio library
+#include <glut.h>								// Header File for glut commands
+#include <btBulletDynamicsCommon.h>				// Header File for Bullet Physics
 
-#include "memoryManager.h"				// Header File for our Memory Manager
-#include "linearAlgebraDLL.h"			// Header File for our math library
-#include "sceneNode.h"					// Header File for the SceneNode/Scenegraph
-#include "sceneObject.h"				// Header File for the SceneObject container
-#include "md2File.h"					// Header File for our md2 loader
-#include "colladaFile.h"				// Header File for out COLLADA loader
-#include "assetManager.h"				// Header File for our Asset Manager
-#include "inputManager.h"				// Header File for our Input Manager
-#include "frameClock.h"					// Header File for our Clock
-#include "sound.h"						// Header File for our Sound
-#include "windowManager.h"				// Header File for our Window Manager
-#include "debugDraw.h"					// Header File for our Debug Drawer 
+#include "memoryManager.h"						// Header File for our Memory Manager
+#include "linearAlgebraDLL.h"					// Header File for our math library
+#include "sceneNode.h"							// Header File for the SceneNode/Scenegraph
+#include "sceneObject.h"						// Header File for the SceneObject container
+#include "md2File.h"							// Header File for our md2 loader
+#include "colladaFile.h"						// Header File for out COLLADA loader
+#include "assetManager.h"						// Header File for our Asset Manager
+#include "inputManager.h"						// Header File for our Input Manager
+#include "frameClock.h"							// Header File for our Clock
+#include "sound.h"								// Header File for our Sound
+#include "windowManager.h"						// Header File for our Window Manager
+#include "debugDraw.h"							// Header File for our Debug Drawer 
 
 using namespace std;
 using namespace linearAlgebraDLL;
@@ -37,8 +37,50 @@ using namespace linearAlgebraDLL;
 class DDEngine
 {
 public:
-	DDEngine(void);
+
+	int screenWidth;							// Window Width
+	int screenHeight;							// Window Height
+	int screenColorDepth;						// Color Depth
+
+	DDEngine(int scrW, int scrH, int colorD);
 	~DDEngine(void);
+
+private:
+
+	// Message pumps used for passing Events between threads
+	MessagePump InputPump;
+	// Pointer to SDL rendering surface
+	SDL_Surface surface;				
+	GLuint image;
+	// Root node and other Scene Node
+	Root rootNode;
+	// Asset manager
+	AssetManager assetManagerPtr;
+	// Physic world
+	btDiscreteDynamicsWorld dynamicsWorld;
+	// Debug Drawing system
+	DebugDraw debugger;
+	// Input manager and Controller
+	inputManager input;
+	Controller controller;
+	// Window Manager definition
+	WindowManager window;
+	// Start the MemoryManager
+	MemoryManager memMgr;
+
+
+	int initGL(void);							// Initialise the rendering window
+	void drawGL(int frameDelta);				// Draw the world
+	float * getCamera(void);					// Move the camera
+	int threadInput(void *data);				// Thread that handles input
+	int threadSound(void *data);				// Thread that handles sounds
+	int threadPhysics(void *data);				// Thread that handles physics
+
+
+
+
+	
+
 };
 
 #endif;
