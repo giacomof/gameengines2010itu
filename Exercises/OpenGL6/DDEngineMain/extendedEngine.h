1942d632347fbbe60262767f3967eabcd399b797
telivyel@gmail.com <telivyel@gmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-11-17 11:00:07 +0000

    added delta time to frame started and frame ended

--- Exercises/OpenGL6/DDEngineMain/extendedEngine.h
+++ Exercises/OpenGL6/DDEngineMain/extendedEngine.h
@@ -7,8 +7,8 @@ public:
 	extendedEngine(int screenWidth, int screenHeight, int colorDepth, bool physic) : DDEngine(screenWidth, screenHeight, colorDepth, physic){};
 
 	void setupScene();
-	void frameStarted();
-	void frameEnded();
+	void frameStarted(int frameDelta);
+	void frameEnded(int frameDelta);
 
 	SceneNode * planeNode;
 	SceneNode * sphereNode;
