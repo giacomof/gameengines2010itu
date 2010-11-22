a8eb650929a16e7967f0c9a9d1ba9529961b3b57
SimWNielsen@hotmail.com <SimWNielsen@hotmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-11-22 11:15:17 +0000

    Animated skeleton that works. Just keyframe stepping atm. No interpolation Still need to do skinning too

--- Exercises/OpenGL5/OpenGL5/sceneObject.cpp
+++ Exercises/OpenGL5/OpenGL5/sceneObject.cpp
@@ -103,7 +103,9 @@ void  ColladaInterface::update(void)
 		if (animationProgress > 1.0f)
 			animationProgress = animationProgress - 1.0f;
 
-		//skeleton->updateSkeleton(currentPose, animationProgress);
+		std::cout << "Root matrix before " << animationProgress << " :\n" << currentPose->jointTransform << "\n\n";
+
+		skeleton->updateSkeleton(currentPose, animationProgress);
 	}
 }
 
