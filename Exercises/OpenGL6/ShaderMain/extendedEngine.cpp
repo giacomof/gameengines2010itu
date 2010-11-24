fa7fcf54b56fc2b19fc53737664650afbdae1c9e
telivyel@gmail.com <telivyel@gmail.com@1a5f623d-5e27-cfcb-749e-01bf3eb0ad9d>
2010-11-24 14:47:54 +0000

    added vertex phong shader and tuned lights

--- Exercises/OpenGL6/ShaderMain/extendedEngine.cpp
+++ Exercises/OpenGL6/ShaderMain/extendedEngine.cpp
@@ -13,9 +13,10 @@ void extendedEngine::setupScene()
 	assetManager.loadTexture("assets/bosscube.jpg", "bossCubeTx");
 	assetManager.loadTexture("assets/duckCM.tga", "duckCM.tga");
 
-	assetManager.createShadingProgram("assets/toon.vert", "assets/toon.frag", "toonShader");
+	//assetManager.createShadingProgram("assets/toon.vert", "assets/toon.frag", "toonShader");
 	assetManager.createShadingProgram("assets/shader1.vert", "assets/shader1.frag", "shader1");
 	assetManager.activateShadingProgram("shader1");
+	//assetManager.activateShadingProgram("toonShader");
 
 	// ******************************
 	// **** CREATE OBJECTS POINT ****
@@ -25,27 +26,31 @@ void extendedEngine::setupScene()
 	SceneObject * plane = this->createPlane(2000.0f, 2000.0f, 0);
 	planeNode = this->addSceneNode(&rootNode, "Plane Node", plane, Vector(0.0f, 0.0f, 0.0f), Vector(0.0f, 0.0f, 0.0f), 1.0f);
 
-	
+	// Create character "rotation center" node
+	SceneObject * sphere = this->createSphere(5, 30, 30, true);
+	sphereNode = this->addSceneNode(&rootNode, "Rotation Center", sphere, Vector(0, 0, 0), Vector(0, 1, 0), 0);
 
-	//// Create Light connected to the battle droid
-	//Light * light1 = (Light*)this->createLight(true, false, 0, 0, 0, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f);
-	//light1Node = this->addSceneNode(battleDroidNode, "Light1 Node", light1, Vector(0, 50, 0), Vector(0, 0, 0), 0);
+	// Create Light connected to the battle droid
+	Light * light1 = (Light*)this->createLight(true, false, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+	light1Node = this->addSceneNode(sphereNode, "Light1 Node", light1, Vector(0, 100, -400), Vector(0, 0, 0), 0);
 
-	// Create Light connected to root
-	Light * light2 = (Light*)this->createLight(true, true, 0.2f,0.2f,0.2f,0.5f,0.5f,0.5f,0.3f,0.3f,0.3f);
-	light2Node = this->addSceneNode(&rootNode, "Light2 Node", light2, Vector(0, 0, 0), Vector(0, 0, 0), 0);
-	light2->setDirection(Vector(0, 1, 0));
+
+
+	//// Create Light connected to root
+	//Light * light2 = (Light*)this->createLight(true, true, 0.2f,0.2f,0.2f,0.5f,0.5f,0.5f,0.3f,0.3f,0.3f);
+	//light2Node = this->addSceneNode(&rootNode, "Light2 Node", light2, Vector(0, 0, 0), Vector(0, 0, 0), 0);
+	//light2->setDirection(Vector(0, 1, 0));
 
 	// Create the teapot
 	SceneObject * teapot = this->createTeapot(50, false);
-	SceneNode * teapotNode = this->addSceneNode(&rootNode, "Teapot Node", teapot, Vector(0.0f, 200.0f, -100.0f), Vector(0.0f,0.0f,0.0f), 0.0f);
+	SceneNode * teapotNode = this->addSceneNode(&rootNode, "Teapot Node", teapot, Vector(0.0f, 45.0f, -100.0f), Vector(0.0f,0.0f,0.0f), 0.0f);
 	//teapotNode->setShadingProgram("toonShader");
 	//teapotNode->setShadingProgram("shader1");
 
 }
 void extendedEngine::frameStarted(int frameDelta)
 {
-
+	sphereNode->rotateAboutAxis(Vector(0, 1, 0), 0.01f * frameDelta);
 }
 void extendedEngine::frameEnded(int frameDelta)
 {
